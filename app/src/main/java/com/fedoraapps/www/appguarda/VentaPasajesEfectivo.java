d2e2d8959ca870e970a3b6e6873288a96f9ac1a4
Maxis <maxis6791@gmail.com>
2016-11-14 16:47:02 -0300

    arreglos

--- app/src/main/java/com/fedoraapps/www/appguarda/VentaPasajesEfectivo.java
+++ app/src/main/java/com/fedoraapps/www/appguarda/VentaPasajesEfectivo.java
@@ -54,6 +54,7 @@ public class VentaPasajesEfectivo extends AppCompatActivity implements View.OnCl
 
     Farcade controller = new Farcade();
     Spinner origen;
+    Farcade farcade = new Farcade();
     DataRecorridoConvertor reco;
     DataEmpleado emp = new DataEmpleado();
     Double distancia;
@@ -139,27 +140,14 @@ public class VentaPasajesEfectivo extends AppCompatActivity implements View.OnCl
 
         System.out.println("DESTINO SELECCIONADO ID !!!----!!!"+Farcade.recorridoSeleccionado.getRecorrido().getId());
 
-
         /* Uso de la clase LocationManager para obtener la localizacion del GPS */
         LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         Localizacion Local = new Localizacion();
-
         Local.setMainActivity(this);
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,
-                (LocationListener) Local);
-
-        Farcade farcade = new Farcade();
+            return;}
+        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0,(LocationListener) Local);
 
         reco = farcade.getRecorridoSeleccionado().getRecorrido();
         Call<DataRecorridoConvertor> call4 = PuntosRecorridoApi.createService().getRecorrido(Farcade.recorridoSeleccionado.getRecorrido().getId());
